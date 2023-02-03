33263893f8b4f68f398d73cc14b93e362350c816
Bhagya Mudgal <bhagyamudgal@gmail.com>
2023-02-04 00:26:15 +0530

    style: added new headings

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import GetAllStreamsSection from "@/components/GetAllStreamsSection";
+import { Heading, Link } from "@chakra-ui/react";
 
 export default function Home() {
     return (
@@ -8,7 +9,29 @@ export default function Home() {
                 <title>
                     Stream PDF | Get Streams Data From streamflow.finance
                 </title>
+                <meta
+                    name="description"
+                    content="Download streams data in PDF format using Stream PDF. You can get streams data from streamflow.finance by using this tool and download it as PDF file. "
+                />
             </Head>
+
+            <Heading as="h1" size="xl" textAlign="center" my={4}>
+                Download streams data in PDF format using Stream PDF.
+            </Heading>
+
+            <Heading as="h2" size="lg" textAlign="center" pb={8}>
+                You can get streams data from{" "}
+                <Link
+                    href="http://streamflow.finance"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    _hover={{ color: "blue.500" }}
+                >
+                    streamflow.finance
+                </Link>{" "}
+                by using this tool and download it as PDF file.
+            </Heading>
+
             <GetAllStreamsSection />
         </div>
     );
